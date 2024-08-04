849e13ba27a5e1c8c76d227ef2928c8b304bec7e
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2024-08-04 13:20:08 -0400

    feat(refactor): :zap: optimize images using the astro Image component

--- src/data.ts
+++ src/data.ts
@@ -1,18 +1,84 @@
-export const skills: string[] = [
-  'react',
-  'next',
-  'jest',
-  'tailwind',
-  'typescript',
-  'javascript',
-  'docker',
-  'dart',
-  'flutter',
-  'elixir',
-  'graphql',
-  'css',
-  'html',
-  'python',
-  'c',
-  'astro',
+import reactImage from './assets/react.svg';
+import nextImage from './assets/next.svg';
+import jestImage from './assets/jest.svg';
+import tailwindImage from './assets/tailwind.svg';
+import typescriptImage from './assets/typescript.svg';
+import javascriptImage from './assets/javascript.svg';
+import dockerImage from './assets/docker.svg';
+import dartImage from './assets/dart.svg';
+import flutterImage from './assets/flutter.svg';
+import elixirImage from './assets/elixir.svg';
+import graphqlImage from './assets/graphql.svg';
+import cssImage from './assets/css.svg';
+import htmlImage from './assets/html.svg';
+import pythonImage from './assets/python.svg';
+import cImage from './assets/c.svg';
+import astroImage from './assets/astro.svg';
+import type { Skill } from './types/skill';
+
+export const skills: Skill[] = [
+  {
+    name: 'React',
+    image: reactImage,
+  },
+  {
+    name: 'Next',
+    image: nextImage,
+  },
+  {
+    name: 'Jest',
+    image: jestImage,
+  },
+  {
+    name: 'Tailwind',
+    image: tailwindImage,
+  },
+  {
+    name: 'TypeScript',
+    image: typescriptImage,
+  },
+  {
+    name: 'JavaScript',
+    image: javascriptImage,
+  },
+  {
+    name: 'Docker',
+    image: dockerImage,
+  },
+  {
+    name: 'Dart',
+    image: dartImage,
+  },
+  {
+    name: 'Flutter',
+    image: flutterImage,
+  },
+  {
+    name: 'Elixir',
+    image: elixirImage,
+  },
+  {
+    name: 'GraphQL',
+    image: graphqlImage,
+  },
+  {
+    name: 'CSS',
+    image: cssImage,
+  },
+  {
+    name: 'HTML',
+    image: htmlImage,
+  },
+  {
+    name: 'Python',
+    image: pythonImage,
+  },
+  {
+    name: 'C',
+    image: cImage,
+  },
+  {
+    name: 'Astro',
+    image: astroImage,
+  },
 ];
