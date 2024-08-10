d3a6999e7380f65518bb42e3348209463f81a63e
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2024-08-10 08:22:13 -0400

    feat(ui): :lipstick: add work experience timeline

---
+++ src/data/experience.ts
@@ -0,0 +1,11 @@
+export const experience = [
+  {
+    title: 'Frontend Engineer',
+    description:
+      'Encargado del desarrollo de la plataforma, los componentes e interfaces gráficos de aplicaciones web. Desarrollando pruebas unitarias y end-to-end para 3 proyectos.',
+    company: 'Avilatek',
+    companyHref: 'https://www.avilatek.com/',
+    startDate: 'Junio 2023',
+    endDate: undefined,
+  },
+];
