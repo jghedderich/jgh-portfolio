0598766b9d943115c314faa66f68131981a4c467
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2024-08-11 12:29:03 -0400

    feat(ui): :sparkles: remove carousel in mobile, doesn't look right. its a feature, not a bug. improve performance

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -2,23 +2,19 @@ import type { Skill } from '../types/skill';
 import {
   astroIcon,
   bootstrapIcon,
-  cIcon,
   cssIcon,
-  dartIcon,
   dockerIcon,
   elixirIcon,
   expressIcon,
   flutterIcon,
   graphqlIcon,
   htmlIcon,
-  javascriptIcon,
   jestIcon,
   mongoIcon,
   nestIcon,
   nextIcon,
   nodeIcon,
   postgresIcon,
-  pythonIcon,
   reactIcon,
   tailwindIcon,
   typescriptIcon,
@@ -28,89 +24,91 @@ export const skills: Skill[] = [
   {
     name: 'React',
     image: reactIcon,
+    type: 'frontend',
   },
   {
     name: 'Next',
     image: nextIcon,
+    type: 'frontend',
   },
   {
     name: 'Jest',
     image: jestIcon,
+    type: 'learning',
   },
   {
     name: 'Tailwind',
     image: tailwindIcon,
+    type: 'learning',
   },
   {
     name: 'TypeScript',
     image: typescriptIcon,
-  },
-  {
-    name: 'JavaScript',
-    image: javascriptIcon,
+    type: 'learning',
   },
   {
     name: 'Docker',
     image: dockerIcon,
-  },
-  {
-    name: 'Dart',
-    image: dartIcon,
+    type: 'learning',
   },
   {
     name: 'Flutter',
     image: flutterIcon,
+    type: 'frontend',
   },
   {
     name: 'Elixir',
     image: elixirIcon,
+    type: 'learning',
   },
   {
     name: 'GraphQL',
     image: graphqlIcon,
+    type: 'frontend',
   },
   {
     name: 'CSS',
     image: cssIcon,
+    type: 'frontend',
   },
   {
     name: 'HTML',
     image: htmlIcon,
-  },
-  {
-    name: 'Python',
-    image: pythonIcon,
-  },
-  {
-    name: 'C',
-    image: cIcon,
+    type: 'frontend',
   },
   {
     name: 'Astro',
     image: astroIcon,
+    type: 'learning',
   },
   {
     name: 'Bootstrap',
     image: bootstrapIcon,
+    type: 'frontend',
   },
   {
     name: 'Express',
     image: expressIcon,
+    type: 'backend',
   },
   {
     name: 'Nest',
     image: nestIcon,
+    type: 'backend',
   },
   {
     name: 'MongoDB',
     image: mongoIcon,
+    type: 'backend',
   },
   {
     name: 'PostgreSQL',
     image: postgresIcon,
+    type: 'backend',
   },
   {
     name: 'Node.js',
     image: nodeIcon,
+    type: 'backend',
   },
 ];
--- src/types/skill.ts
+++ src/types/skill.ts
@@ -1,4 +1,7 @@
+type skillType = 'learning' | 'frontend' | 'backend' | 'tools';
+
 export type Skill = {
   name: string;
   image: ImageMetadata;
+  type: skillType;
 };
