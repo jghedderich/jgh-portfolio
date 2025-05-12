b43cdecd9eb0c1cb52845d1e0f60502b5d019757
Juan Hedderich <hedderich.dev@gmail.com>
2025-05-11 21:18:55 -0400

    upgrade Astro version, remove bad alt texts, fixes for a11y

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,6 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-
     extend: {
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
